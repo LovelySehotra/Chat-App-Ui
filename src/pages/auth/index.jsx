c1865c1ad24c36584fa8803b8c77f745a36485d8
Lovely Sehotra <lsehotra12@gmail.com>
2024-07-27 20:43:07 +0530

    Setuo Client side and Auth Page Ui

---
+++ src/pages/auth/index.jsx
@@ -0,0 +1,57 @@
+import victory from "@/assets/victory.svg"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { useState } from "react";
+
+function Auth() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const handleLogin =async()=>{
+
+    }
+    const handleSignUp =async()=>{
+
+    }
+    return (
+        <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+            <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid ">
+                <div className="flex items-center justify-center flex-col">
+                    <div className="flex items-center justify-center flex-col">
+                        <div className="flex items-center justify-center ">
+                            <h1 className="text-4xl font-bold md:text-6xl" >Welcome</h1>
+                            <img src={victory} alt="victory emoji" className="h-[80px]" />
+                        </div>
+                        <p className="font-medium text-center">
+                            Fill the detail to get Started this best chat app
+
+                        </p>
+                        <div className="flex items-center justify-center w-full">
+                            <Tabs className="w-3/4">
+                                <TabsList className="bg-transparent rounded-none w-full">
+                                    <TabsTrigger value="login" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300 ">Log In</TabsTrigger>
+                                    <TabsTrigger value="signup" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300 ">Sign Up</TabsTrigger>
+                                </TabsList>
+                                <TabsContent className=" flex flex-col gap-5 mt-10" value="login">
+
+                                    <Input placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="rounded-full p-6" />
+                                    <Input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="rounded-full p-6" />
+<Button className="rounded-full p-6" onClick={handleLogin} >LogIn</Button>
+                                </TabsContent>
+                                <TabsContent className=" flex flex-col gap-5 " value="signup">
+                                    <Input placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="rounded-full p-6"/>
+                                    <Input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="rounded-full p-6" />
+                                    <Input placeholder="Confirm Password" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="rounded-full p-6" />
+                                    <Button className="rounded-full p-6" onClick={handleSignUp} >SignUp</Button>
+                                </TabsContent>
+                            </Tabs>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Auth
